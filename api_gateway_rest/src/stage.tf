7a32986505f91e3f85deccf1472ecb6e07d7c93a
Jayakumar Jayaraman <jjayaraman@onvutech.com>
2022-10-23 02:55:05 +0100

    method settings added

--- api_gateway_rest/src/stage.tf
+++ api_gateway_rest/src/stage.tf
@@ -4,3 +4,16 @@ resource "aws_api_gateway_stage" "hello" {
   deployment_id = aws_api_gateway_deployment.hello.id
   rest_api_id   = aws_api_gateway_rest_api.hello.id
 }
+
+
+resource "aws_api_gateway_method_settings" "all" {
+
+  stage_name  = aws_api_gateway_stage.hello.stage_name
+  rest_api_id = aws_api_gateway_rest_api.hello.id
+  method_path = "*/*"
+
+  settings {
+    metrics_enabled = true
+  }
+
+}
